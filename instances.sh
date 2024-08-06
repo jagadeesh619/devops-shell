a710ca25d928ed073e222f692d6060c2bcbc15e8
jagadeeshwar <mummadijagadeesh143@gmail.com>
2024-08-06 09:21:20 +0530

    creating ec2 via aws cli

--- instances.sh
+++ instances.sh
@@ -12,11 +12,24 @@ do
     else
         INSTANCES_TYPE="t2.micro"
     fi
-    if [ $i == "web" ];then
-        IP_Address=$(aws ec2 run-instances --image-id $AMI --instance-type $INSTANCES_TYPE --security-group-ids $SG --count 1 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PublicIpAddress' --output text)
+    # if [ $i == "web" ];then
+    #     IP_Address=$(aws ec2 wait run-instances --image-id $AMI --instance-type $INSTANCES_TYPE --security-group-ids $SG --count 1 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PublicIpAddress' --output text)
+    # else
+    #     IP_Address=$(aws ec2 run-instances --image-id $AMI --instance-type $INSTANCES_TYPE --security-group-ids $SG --count 1 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
+    # fi
+# Launch the instance
+    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids $SG --count 1 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].InstanceId' --output text)
+
+    # Wait until the instance is running
+    aws ec2 wait instance-running --instance-ids $INSTANCE_ID
+
+    # Get the IP address
+    if [ "$i" = "web" ]; then
+        IP_Address=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
     else
-        IP_Address=$(aws ec2 run-instances --image-id $AMI --instance-type $INSTANCES_TYPE --security-group-ids $SG --count 1 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
+        IP_Address=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
     fi
+    
     echo "Creating Route53 record for $i APPLICATION : {$IP_Address} "
     aws route53 change-resource-record-sets \
     --hosted-zone-id Z092833841YAHBL3RZKX \
